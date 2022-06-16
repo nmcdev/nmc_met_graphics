fb715c8b87f70e8bda680035ca4be37397e847bb
NMC-DAVE <daikan1998@163.com>
2022-06-16 21:50:38 +0800

    Add tcmarkers for ploting tropical cyclone symbol.

--- nmc_met_graphics/plot/util.py
+++ nmc_met_graphics/plot/util.py
@@ -565,6 +565,83 @@ def add_mslp_label(ax, proj_ccrs, mslp, lat, lon):
                 b.set_path_effects([mpatheffects.Stroke(linewidth=1.0, foreground='black'),
                        mpatheffects.SimpleLineShadow(),mpatheffects.Normal()])
                 xyplotted.append((x,y))
+                
+                
+def plot_maxmin_points(ax, lon, lat, data, extrema, nsize, symbol, color='k',
+                       plotValue=True, transform=None):
+    """
+    This function will find and plot relative maximum and minimum for a 2D grid. The function
+    can be used to plot an H for maximum values (e.g., High pressure) and an L for minimum
+    values (e.g., low pressue). It is best to used filetered data to obtain  a synoptic scale
+    max/min value. The symbol text can be set to a string value and optionally the color of the
+    symbol and any plotted value can be set with the parameter color.
+    
+    https://github.com/hojjunekim/WeatherMap/blob/master/upper-level%20weather%20map/HW2_final_code.ipynb
+
+    Parameters
+    ----------
+        lon : 2D array
+            Plotting longitude values
+        lat : 2D array
+            Plotting latitude values
+        data : 2D array
+            Data that you wish to plot the max/min symbol placement
+        extrema : str
+            Either a value of max for Maximum Values or min for Minimum Values
+        nsize : int
+            Size of the grid box to filter the max and min values to plot a reasonable number
+        symbol : str
+            Text to be placed at location of max/min value
+        color : str
+            Name of matplotlib colorname to plot the symbol (and numerical value, if plotted)
+        plot_value : Boolean (True/False)
+            Whether to plot the numeric value of max/min point
+
+    Return
+    ------
+        The max/min symbol will be plotted on the current axes within the bounding frame
+        (e.g., clip_on=True)
+        
+    Example
+    ------
+    #plot H/L symbols
+    plot_maxmin_points(lons, lats, hght_500, 'max', 3,
+                    symbol='H', color='blue', transform=ccrs.PlateCarree())
+    plot_maxmin_points(lons, lats, hght_500, 'min', 3,
+                    symbol='L', color='red', transform=ccrs.PlateCarree())
+
+    #plot W/C symbols
+    plot_maxmin_points(lons, lats, temp_500.to('degC').m, 'max', 3,
+                    symbol='W', color='red', transform=ccrs.PlateCarree())
+    plot_maxmin_points(lons, lats, temp_500.to('degC').m, 'min', 3,
+                    symbol='C', color='blue', transform=ccrs.PlateCarree())
+    """
+
+    if (extrema == 'max'):
+        data_ext = maximum_filter(data, nsize, mode='nearest')
+    elif (extrema == 'min'):
+        data_ext = minimum_filter(data, nsize, mode='nearest')
+    else:
+        raise ValueError('Value for hilo must be either max or min')
+
+    if lon.ndim == 1:
+        lon, lat = np.meshgrid(lon, lat)
+
+    mxx, mxy = np.where(data_ext == data)
+
+    for i in range(len(mxy)):
+        ax.text(lon[mxx[i], mxy[i]], lat[mxx[i], mxy[i]], symbol, color=color, size=36,
+                clip_on=True, horizontalalignment='center', verticalalignment='center',
+                transform=transform)
+        if plotValue:
+            ax.text(lon[mxx[i], mxy[i]], lat[mxx[i], mxy[i]],
+                    '\n' + str(np.int(data[mxx[i], mxy[i]])),
+                    color=color, size=12, clip_on=True, fontweight='bold',
+                    horizontalalignment='center', verticalalignment='top', transform=transform)
+        ax.plot(lon[mxx[i], mxy[i]], lat[mxx[i], mxy[i]], marker='o', markeredgecolor='black',
+                markerfacecolor='white', transform=transform)
+        ax.plot(lon[mxx[i], mxy[i]], lat[mxx[i], mxy[i]],
+                marker='x', color='black', transform=transform)
 
 
 #======================================
